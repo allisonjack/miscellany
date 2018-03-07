6fb8379098f6a5d0f9289db3517c075846463925
Allison Jack <allison.e.jack@gmail.com>
2018-03-07 10:54:53 -0500

    Create startup.m

---
+++ startup.m
@@ -0,0 +1,5 @@
+addpath(fullfile('/','Users','allisonjack','Documents','MATLAB','spm12'));
+addpath(fullfile('/','Users','allisonjack','Documents','MATLAB','fmripower'));
+addpath(fullfile('/','Users','allisonjack','Documents','MATLAB','paruly','paruly'));
+addpath(fullfile('/','Users','allisonjack','Documents','MATLAB','PowerMap'));
+addpath(fullfile('/','Users','allisonjack','Documents','MATLAB','NIfTI_20140122'));
